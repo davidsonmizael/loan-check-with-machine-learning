68e2ecb8af519d79b815d9405f684d9a1b668782
Davidson Mizael <davidsonmizael@gmail.com>
2022-04-07 07:49:36 -0300

    Updating run to only start the server when running the file directly

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ load_dotenv('.env')
 
 app = init_app()
 
-host_ip = os.getenv('FLASK_HOST')
-host_port = os.getenv('FLASK_PORT')
-app.run(host=host_ip, port=host_port)
+if __name__ == '__main__':
+    host_ip = os.getenv('FLASK_HOST')
+    host_port = os.getenv('FLASK_PORT')
+    app.run(host=host_ip, port=host_port)
